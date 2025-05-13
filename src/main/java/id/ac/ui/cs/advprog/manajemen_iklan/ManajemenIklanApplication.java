dbd4dfbb5e410158ec529ea493d0f1e36245b05c
moikelaff <dylan.adiprawira@gmail.com>
2025-05-13 14:06:38 +0700

    feat: init repo

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/ManajemenIklanApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.manajemen_iklan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenIklanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajemenIklanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/manajemen_iklan/ManajemenIklanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.manajemen_iklan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajemenIklanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
