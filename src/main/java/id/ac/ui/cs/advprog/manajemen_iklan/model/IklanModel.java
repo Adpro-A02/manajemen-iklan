fe4ee492bbb46f8002bfbae5b607909424bd6bf0
moikelaff <dylan.adiprawira@gmail.com>
2025-05-13 14:38:46 +0700

    feat: [red] added models

--- src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/model/IklanModel.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/model/IklanModel.java
@@ -1,5 +1,67 @@
 package id.ac.ui.cs.advprog.manajemen_iklan.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
+import java.time.LocalDateTime;
+
+import id.ac.ui.cs.advprog.manajemen.iklan.enums.IklanStatus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "advertisements")
 public class IklanModel {
     
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private String imageUrl;
+    
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+    
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private IklanStatus status;
+    
+    @Column
+    private String clickUrl;
+    
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+    
+    // Pre-persist and pre-update methods to set timestamps
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+    
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
