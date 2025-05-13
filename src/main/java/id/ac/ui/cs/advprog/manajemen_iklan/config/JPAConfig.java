7dfd7686e3f7dab5c5ce3d93428a363cf86b6530
moikelaff <dylan.adiprawira@gmail.com>
2025-05-13 15:36:38 +0700

    feat: added jpa config

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/config/JPAConfig.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.manajemen_iklan.config;
+
+import jakarta.persistence.EntityListeners;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "id.ac.ui.cs.advprog.manajemen.iklan.repository")
+public class JPAConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication == null || !authentication.isAuthenticated()) {
+                return Optional.of("system");
+            }
+            return Optional.of(authentication.getName());
+        };
+    }
+}
