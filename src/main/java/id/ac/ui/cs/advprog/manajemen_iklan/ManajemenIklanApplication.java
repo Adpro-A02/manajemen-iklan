ae108266752b0e6f057646f3ab61680e5551e31f
moikelaff <dylan.adiprawira@gmail.com>
2025-05-14 12:28:11 +0700

    feat: added caching

--- src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/ManajemenIklanApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/ManajemenIklanApplication.java
@@ -2,12 +2,13 @@ package id.ac.ui.cs.advprog.manajemen_iklan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ManajemenIklanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManajemenIklanApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ManajemenIklanApplication.class, args);
+    }
 }
