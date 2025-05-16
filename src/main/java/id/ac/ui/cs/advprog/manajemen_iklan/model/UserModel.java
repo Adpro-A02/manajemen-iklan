c77c229757488c14da1d4de94fc48bede8357890
moikelaff <dylan.adiprawira@gmail.com>
2025-05-16 12:03:38 +0700

    fix: moved auth model to user model

--- src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/model/UserModel.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemen_iklan/model/UserModel.java
@@ -1,70 +1,26 @@
 package id.ac.ui.cs.advprog.manajemen_iklan.model;
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.UuidGenerator;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "users")
-@EntityListeners(AuditingEntityListener.class)
 public class UserModel implements UserDetails {
-    
-    @Id
-    @GeneratedValue
-    @UuidGenerator
-    private String id;
-    
-    @Column(nullable = false, unique = true)
     private String username;
-    
-    @Column(nullable = false)
     private String password;
-    
-    @Column(nullable = false)
     private String name;
-    
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
-    @Builder.Default
-    private Set<String> roles = new HashSet<>();
-    
-    @Builder.Default
-    private boolean enabled = true;
-    
-    @CreatedDate
-    private LocalDateTime createdAt;
-    
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private List<String> roles;
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -90,6 +46,6 @@ public class UserModel implements UserDetails {
     
     @Override
     public boolean isEnabled() {
-        return enabled;
+        return true;
     }
 }
